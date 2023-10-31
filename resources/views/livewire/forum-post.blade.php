08ad56e3493f462e4730eb1e017dec51ca4ffc0f
ArifulSikder <mdislamnoyon3@gmail.com>
2023-11-01 00:01:42 +0600

    Design of forum

--- resources/views/livewire/forum-post.blade.php
+++ resources/views/livewire/forum-post.blade.php
@@ -1,7 +1,7 @@
 <div class="gallery-post-section pt-4">
     <div class="row">
         @forelse ($posts as $post)
-            <div class="col-lg-8 offset-2 pb-md-3">
+            <div class="col-lg-8 offset-lg-2 col-md-12 col-sm-12 pb-md-3">
                 <div class="card custom-card mb-4 h-100 border">
                     <div class="image-container p-2">
                         <div class="author-info avater-top">
@@ -11,8 +11,31 @@
                             <span class="created-time">Posted
                                 {{ $post->created_at->diffForHumans() }}</span>
                         </div>
-                        <div class="author-info avater-top">
-                            @if ($post->file_type == 1)
+                        <div class="author-info avater-top" style="height: 75vh; cursor: pointer;">
+                            <div class="row">
+                                <div class="col-lg-6 px-2 pl-2">
+                                    <img height="340" width="100%" src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                        class="img-fluida mt-2" alt="Post Image 1">
+                                </div>
+                                <div class="col-lg-6 px-2">
+                                    <img height="340" width="100%" src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                        class="img-fluida mt-2" alt="Post Image 1">
+                                </div>
+                                <div class="col-lg-6 px-2 pr-2">
+                                    <img height="340" width="100%" src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                        class="img-fluida mt-2" alt="Post Image 1">
+                                </div>
+                                <div class="col-lg-6 px-2">
+                                    <img height="340" width="100%" src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                        class="img-fluida mt-2" alt="Post Image 1">
+                                </div>
+                                {{-- <div class="col-lg-12 mt-3 mt-md-3 col-md-12">
+                                    <img height="340" width="100%" src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                        class="img-fluida" alt="Post Image 1">
+                                </div> --}}
+                            </div>
+
+                            {{-- @if ($post->file_type == 1)
                                 <img src="{{ asset($post->file) }}"
                                     wire:click.prevent="showInModal({{ $post }})" class="card-img-top"
                                     style="height: 75vh; cursor: pointer;" alt="Post Image">
@@ -21,7 +44,7 @@
                                     <source src="{{ asset($post->file) }}" type="video/mp4">
                                     Your browser does not support the video tag.
                                 </video>
-                            @endif
+                            @endif --}}
                         </div>
                         <div class="post-content p-2">
                             <h2 class="post-title">{{ $post->title }}</h2>
@@ -104,23 +127,25 @@
         <div>{{ $posts->links() }}</div>
 
     </div>
-    <div class="modal" tabindex="-1" role="dialog" id="showPhoto" >
+    <div class="modal" tabindex="-1" role="dialog" id="showPhoto">
         <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="btn btn-danger btn-sm float-right rounded" wire:click.prevent="hideImageModal" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            <div class="modal-body">
-                <img src="" id="imageId" class="img-fluid" alt="">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn btn-danger btn-sm float-right rounded"
+                        wire:click.prevent="hideImageModal" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="" id="imageId" class="img-fluid" alt="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" wire:click.prevent="hideImageModal"
+                        data-dismiss="modal">Close</button>
+                </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" wire:click.prevent="hideImageModal" data-dismiss="modal">Close</button>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
 
 
     @push('script')
@@ -131,9 +156,90 @@
             }, false);
 
             window.addEventListener('hide-form', (message) => {
-                $('#imageId').attr('src',"");
+                $('#imageId').attr('src', "");
                 $('#showPhoto').modal('hide');
             }, false);
         </script>
     @endpush
 </div>
+
+{{-- <div class="gallery-page">
+    <section class="gallery-section py-5">
+        <div class="container">
+            <div class="gallery-post-section pt-4">
+                <div class="row">
+                    <div class="col-lg-8 offset-4 pb-md-3">
+                        <div class="card custom-card mb-4 h-100">
+                            <div class="image-container">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <img src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                            class="img-fluid" alt="Post Image 1">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <img src="http://127.0.0.1:8000/uploads/post%20image/39/large_289447134_6000595216634026_6797221462978289858_n_72a1410d74_f39d3b3f8d.jpg"
+                                            class="img-fluid" alt="Post Image 2">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="author-info">
+                                    <img src="profile-picture.jpg" class="profile-picture" alt="Profile Picture">
+                                    <span class="author-name">John Doe</span>
+                                    <span class="created-time">Posted 2 hours ago</span>
+                                </div>
+                                <h2 class="post-title">Sample Forum Post Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod tincidunt libero
+                                    ac aliquam.</p>
+                                <div class="d-flex justify-content-between">
+                                    <!-- Like button with Font Awesome heart icon -->
+                                    <button class="btn btn-primary">
+                                        <i class="fas fa-heart"></i> Like
+                                    </button>
+                                    <button class="btn btn-secondary toggle-comments">Comment</button>
+                                </div>
+                                <div class="comments comment-section">
+                                    <ul>
+                                        <li class="comment">
+                                            <div class="comment-details">
+                                                <div class="author-info">
+                                                    <img src="profile-picture.jpg" class="profile-picture"
+                                                        alt="Profile Picture">
+                                                    <span class="author-name">John Doe</span>
+                                                    <span class="created-time">Posted 2 hours ago</span>
+                                                </div>
+                                                <p class="comment-text">Comment 1</p>
+                                            </div>
+                                        </li>
+                                        <li class="comment">
+                                            <div class="comment-details">
+                                                <div class="author-info">
+                                                    <img src="profile-picture.jpg" class="profile-picture"
+                                                        alt="Profile Picture">
+                                                    <span class="author-name">John Doe</span>
+                                                    <span class="created-time">Posted 2 hours ago</span>
+                                                </div>
+                                                <p class="comment-text">Comment 2</p>
+                                            </div>
+                                        </li>
+                                        <!-- Add more comments as needed -->
+                                    </ul>
+                                    <button class="btn btn-info see-more-btn" style="display: block;">See
+                                        More</button>
+                                </div>
+                                <div class="comment-form">
+                                    <form>
+                                        <div class="form-group border">
+                                            <textarea class="form-control comment-input" placeholder="Add a comment"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div> --}}
